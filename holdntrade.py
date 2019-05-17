0a2da66bd3561244bcd0a4d7573276997faa51aa
jaNGOB <gobeli@hotmail.ch>
2019-05-17 16:52:21 +0200

    Update holdntrade.py

--- holdntrade.py
+++ holdntrade.py
@@ -515,12 +515,8 @@ if __name__ == '__main__':
         balance = get_balance()
         amount = round(balance / divider * price)
         first_amount = round(balance / 2 * price)
-        
-        if amount < int(threshold):
-            #send-email / restart
-            pass
 
-        elif loop:
+        if loop:
             trade_executed(price, amount)
             sell_executed(price, amount)
 
